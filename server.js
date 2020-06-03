71711b5bc77d4479852a38dc7b110a57aeddd6ca
Eduardo Goes <eduardogoes11@gmail.com>
2020-06-02 23:49:47 -0300

    server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV === "development";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const port = process.env.PORT_SITE || 3000;
+const port = process.env.PORT || 1337;
 
 (async () => {
   try {
