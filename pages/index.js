9cb481d476191847bd7f71b8b2c91d81e38b818e
Eduardo Goes <eduardogoes11@gmail.com>
2020-06-05 14:42:34 -0300

    remove getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 import Head from "next/head";
 
-function Home({ repositories }) {
+function Home() {
   return (
     <div id="page-root">
       <Head>
@@ -13,18 +13,8 @@ function Home({ repositories }) {
 
       <h1>GGlass</h1>
       <h2>Em construção</h2>
-
-      {repositories.map((repo) => (
-        <p key={repo.id}>{repo.name}</p>
-      ))}
     </div>
   );
 }
-
-Home.getInitialProps = async (ctx) => {
-  const response = await fetch("https://api.github.com/orgs/rocketseat/repos");
-
-  return { repositories: await response.json() };
-};
 
 export default Home;
